525c5289a70f5e816d7fb758dd65f27c2a9ace21
tanadaaa <autumn.jaxy@gmail.com>
2024-03-19 12:19:48 +0900

    requirements.txt更新

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 # __init__.py
 
 from flask import Flask
-from .views import chatbot_response, get_chat_history, on_chat_start
+from .views import chatbot_response, on_chat_start
 
 app = Flask(__name__)
 app.route("/chatbot", methods=["POST"])(chatbot_response)
-app.route("/history", methods=["GET"])(get_chat_history)
 app.route("/start", methods=["POST"])(on_chat_start)
