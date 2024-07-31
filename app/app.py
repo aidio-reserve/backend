7806bdfa03c9d9eb776eb1c7bb4967b87d5f0a20
yumayuma708 <yumaskywalker708@gmail.com>
2024-07-31 16:26:25 +0900

    CORSを設定

--- app/app.py
+++ app/app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 from app.bot import core
 import json
 from pathlib import Path
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route("/")
 def index():
